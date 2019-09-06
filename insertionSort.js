cdbea4e38209c176484c2c4a8bcc89b6dd1ccb08
Behlul <bvaliyev96@gmail.com>
2019-09-06 17:51:33 +0400

    insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,7 +1,7 @@
 var array = [9, 8, 5, 90, 2, 4, 56, 5, 8, 6, 25, 10],
     key;
 
-for (i = 0; i < array.length; i++) {
+for (i = 1; i < array.length; i++) {
     key = array[i];
     j = i - 1;
     while (j >= 0 && array[j] > key) {
