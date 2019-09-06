346e3c9db807a8ee6996c365a7a9fc45fdb619e5
Behlul <bvaliyev96@gmail.com>
2019-09-06 16:32:33 +0400

    insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+var array = [9, 8, 5, 90, 2, 4, 56, 5, 8, 6, 25, 10],
+    key;
+
+for (i = 0; i < array.length; i++) {
+    key = array[i];
+    j = i - 1;
+    while (j >= 0 && array[j] > key) {
+        array[j + 1] = array[j];
+        j = j - 1;
+    }
+    array[j + 1] = key
+}
+console.log(array);
